c5d5350ccae4afc11e804704d49f5315460fbc68
mathiasbarreto <mathiasbarreto.dev@gmail.com>
2021-08-02 12:39:18 -0300

    buscador cursos alura

--- composer-project/buscador-cursos-alura/buscar-cursos.php
+++ composer-project/buscador-cursos-alura/buscar-cursos.php
@@ -1,3 +1,18 @@
-$client = new \GuzzleHttp\Client();
-$response = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
-$html = $response->getBody();
+<?php
+
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
